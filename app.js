8cd32f559667757eb37007557dc5c6146294fb58
Abdullahkimrigh <abdullahkimregh@gmail.com>
2023-05-17 16:27:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log(x);
+
+var x = "hello";
+var y = "Abdullah";
+
+console.log(5+5); //Arthemtic operations
+
+var z = 100
+console.log(10+z);
+console.log(10-z);
+console.log(10*z);
+console.log(10/z);
+console.log(10%z);
+console.log(10**z);
+
+z+=1
+// x++
+console.log(z);
+
+console.log(x+" 10"); //concat
+
+console.log(typeof x); //define data type of the variable
+console.log(typeof +x);
+console.log(typeof "hello");
+console.log(typeof 10.6);
+
+console.log(x +" "+ y.substring(0, 1)); //السلاسل النصية //function
+console.log(y.length); //خاصية
+console.log(y.toUpperCase()); //الحرف الكبير //function
+console.log(y.toLowerCase()); //الحرف الصغير //function
+console.log(y.trim()); //حذف الحروف الاولى والاخيرة //function
+console.log(y.trimStart()); //حذف الحروف الاولى //function
+console.log(y.trimEnd()); //حذف الحروف الاخيرة //function
+console.log(y.indexOf("A")); //العنصر الذي يحتوي على هذا الحرف //function
+console.log(y.includes("A")); //العنصر الذي يحتوي على هذا الحرف //function
+console.log(y.replace("A", "a")); //العنصر الذي يحتوي على هذا الحرف //function
+console.log(y.charAt(1)); //العنصر الذي يحتوي على هذا الحرف //function
+
+var name = 'Abdullah';a
+console.log(`Hello ${name}, welcome to my website`);
+
